e3ec3cf2a9a66f478adf1b91b8cc1d322fab5421
sindpet <137641233+sindpet@users.noreply.github.com>
2023-09-14 22:37:34 +0200

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,70 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Constants
+PROJECTS = 10
+TASKS = 20
+TAGS = 8
+
+# Cleanup previous data
+TaskTag.destroy_all
+Tag.destroy_all
+Task.destroy_all
+Project.destroy_all
+User.destroy_all
+
+# Create users
+user1 = User.create(first_name: "This is a",
+                    last_name: "Test",
+                    email: "test@gmail.com",
+                    password: "taskspass",
+                    password_confirmation: "taskspass")
+user2 = User.create(first_name: "Another",
+                    last_name: "Test",
+                    email: "test2@gmail.com",
+                    password: "taskspass",
+                    password_confirmation: "taskspass")
+
+# Create projects
+[user1, user2].each do |user|
+  PROJECTS.times do |index|
+    Project.create(title: "Project #{index + 1}",
+                   position: Faker::Number.between(from: 1, to: TASKS).to_i,
+                   user: user)
+  end
+end
+
+# Create tasks
+[user1, user2].each do |user|
+  TASKS.times do |index|
+    Task.create(title: "Task #{index + 1}",
+                description: Faker::Lorem.paragraph,
+                is_done: Faker::Boolean.boolean,
+                user: user)
+  end
+end
+
+# Assign some tasks to some projects
+[user1, user2].each do |user|
+  project_ids = user.projects.pluck(:id)
+  3.times do
+    random_task = user.tasks[rand(TASKS)]
+    random_project_id = project_ids.sample
+    random_task.update(project_id: random_project_id)
+  end
+end
+
+# Create tags
+[user1, user2].each do |user|
+  TAGS.times do
+    Tag.create(title: Faker::Creature::Animal.name, user: user)
+  end
+end
+
+# Assign some tags to some tasks
+[user1, user2].each do |user|
+  7.times do
+    user.tasks[rand(TASKS)].tags << user.tags[rand(TAGS)]
+  end
+end
