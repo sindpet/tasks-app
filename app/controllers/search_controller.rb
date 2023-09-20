29df464532ff746f6edc1276cc701202dde31c66
sindpet <137641233+sindpet@users.noreply.github.com>
2023-09-20 17:53:24 +0200

    Change query to use string interpolation instead of concatenation

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
   def index
     if params[:q]
-      sanitized_query = "%" + ActiveRecord::Base::sanitize_sql_like(params[:q]) + "%"
+      sanitized_query = "%#{ActiveRecord::Base::sanitize_sql_like(params[:q])}%"
+
       @projects = current_user.projects.where("title ILIKE ?", sanitized_query)
       @tasks = current_user.tasks.where("title ILIKE ?", sanitized_query)
       @tags = current_user.tags.where("title ILIKE ?", sanitized_query)
